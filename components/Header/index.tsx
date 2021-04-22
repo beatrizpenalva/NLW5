406e3f53d36be5a854828ae94e46249b26d4cafc
Beatriz Penalva <biapenalva@gmail.com>
2021-04-22 09:44:11 -0300

    style Header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,4 @@
+import styles from "./styles.module.scss";
 import format from "date-fns/format";
 import ptBR from "date-fns/locale/pt-BR";
 
@@ -7,10 +8,10 @@ export default function Header() {
     });
 
     return (
-        <header>
+        <header className={styles.headerContainer}>
             <img src="/logo.svg" alt="Headphone da logo Podcastr" />
-            <p>O melhor para você ouvir, sempre.</p>
-            <p>{currentDate}</p>
+            <p> O melhor para você ouvir, sempre </p>
+            <span> {currentDate} </span>
         </header>
     );
 }
