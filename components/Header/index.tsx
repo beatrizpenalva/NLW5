0f4270acc6adb9c0f7ea2c1880528c810aa9c8b4
Beatriz Penalva <biapenalva@gmail.com>
2021-04-22 09:20:44 -0300

    feature/date-implement

---
+++ components/Header/index.tsx
@@ -0,0 +1,16 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+export function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMM', {
+        locale: ptBR,
+    });
+
+    return (
+        <header>
+            <img src="/logo.svg" alt="Headphone da logo Podcastr" />
+            <p>O melhor para você ouvir, sempre.</p>
+            <p>{currentDate}</p>
+        </header>
+    );
+}
